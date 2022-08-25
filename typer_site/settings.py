ae056bd2879b1fce8fea2dbbfe8a4d2cf183abaf
Sander Buruma <sanderburuma@fastmail.nl>
2022-08-25 23:20:46 +0200

    1. ignore dotfiles 2. settings doesnt throw error on empty key

--- typer_site/settings.py
+++ typer_site/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['secret_key']
+SECRET_KEY = os.environ.get('secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('debug')) or False
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'typer_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'OPTIONS': {
+            'service': 'my_service',
+            'passfile': '.my_pgpass'
+        }
     }
 }
 
