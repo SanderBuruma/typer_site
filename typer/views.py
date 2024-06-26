ec27225454b7ddfdfcc1c36224a86fb8bef39924
Sander Buruma <sburuma@citizengo.net>
2024-06-26 22:10:02 +0200

    removedo 2 useless comments

--- typer/views.py
+++ typer/views.py
@@ -17,9 +17,7 @@ class IndexView(TemplateView):
     template_name = 'index.html'
 
     def get_context_data(self, **kwargs):
-        # Call the base implementation first to get a context
         context = super().get_context_data(**kwargs)
-        # Add in a QuerySet of all the books
         context["book_list"] = Book.objects.all()
         return context
 
